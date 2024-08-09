92839e428c31ed61be2b284580d445a9a256e272
raihanuldev <rihanthedev@gmail.com>
2024-08-10 00:02:30 +0600

    refector code routes

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -4,22 +4,73 @@ import CreateFaculty from "../pages/admin/CreateFaculty";
 import CreateStudent from "../pages/admin/CreateStudent";
 
 
-export const adminPaths = [
-        
-    {
-      path: "dashbord",
-      element: <AdminDashbord />,
-    },
-    {
-      path: "create-admin",
-      element: <CreateAdmin />,
-    },
-    {
-      path: "create-faculty",
-      element: <CreateFaculty />,
-    },
-    {
-      path: "create-student",
-      element: <CreateStudent />,
+type Route ={
+    path: string;
+    element: JSX.Element;
+}
+
+const adminPaths2 = [
+  {
+    name: "Dashbord",
+    path: "dashbord",
+    element: <AdminDashbord />,
+  },
+  {
+    name: "Management",
+    children: [
+      {
+        name: "Create Admin",
+        path: "create-admin",
+        element: <CreateAdmin/>,
+      },
+      {
+        name: "Create Faculty",
+        path: "create-faculty",
+        element: <CreateFaculty/>,
+      },
+      {
+        name: "Create Student",
+        path: "create-student",
+        element: <CreateStudent/>,
+      },
+    ],
+  },
+];
+
+export const adminRoutes = adminPaths2.reduce((acc, item) => {
+
+    if(item.path && item.element){
+        acc.push({
+            path:item.path,
+            element:item.element
+        })
     }
-  ]
+    if(item.children){
+        item.children.forEach((child)=>{
+            acc.push({
+                path: child.path,
+                element:child.element,
+            })
+        })
+    }
+  return acc;
+}, [] as Route[]);
+console.log(adminRoutes);
+// export const adminPaths = [
+//   {
+//     path: "dashbord",
+//     element: <AdminDashbord />,
+//   },
+//   {
+//     path: "create-admin",
+//     element: <CreateAdmin />,
+//   },
+//   {
+//     path: "create-faculty",
+//     element: <CreateFaculty />,
+//   },
+//   {
+//     path: "create-student",
+//     element: <CreateStudent />,
+//   },
+// ];
--- test.js
+++ test.js
@@ -1,7 +1,47 @@
-const arr = [2, 3, 4, 5, 6, 7, 8];
+const arr = [
+  {
+    name: "Dashbord",
+    path: "dashbord",
+    element: "ADMIN_DASHBORD",
+  },
+  {
+    name: "Management",
+    children: [
+      {
+        name: "Create Admin",
+        path: "create-admin",
+        element: "CREATE_ADMIN",
+      },
+      {
+        name: "Create Faculty",
+        path: "create-faculty",
+        element: "CREATE_FACULTY",
+      },
+      {
+        name: "Create Admin",
+        path: "create-student",
+        element: "CREATE_STUDENT",
+      },
+    ],
+  },
+];
 
 const result = arr.reduce((acc, item) => {
-  acc.push(item);
+
+    if(item.path && item.element){
+        acc.push({
+            path:item.path,
+            element:item.element
+        })
+    }
+    if(item.children){
+        item.children.forEach((child)=>{
+            acc.push({
+                path: child.path,
+                element:child.element,
+            })
+        })
+    }
   return acc;
 }, []);
 
