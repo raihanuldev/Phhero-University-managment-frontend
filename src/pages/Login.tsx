839f12710700f216b27b5da7e94998c463def386
raihanuldev <rihanthedev@gmail.com>
2024-08-11 06:48:37 +0600

    login form fokira

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+    const {register, handleSubmit} = useForm()
+
+    const onsubmit =(data)=>{
+        console.log(data);
+    }
     return (
-        <div>
-            this is Login COmpnets
-        </div>
+        <form onClick={handleSubmit(onsubmit)}>
+            <div>
+                <label htmlFor="id">ID:</label>
+                <input type="text" id="id" {...register('id')} />
+            </div>
+            <div>
+                <label htmlFor="password">Password:</label>
+                <input type="text" id="password" {...register('password')}/>
+            </div>
+            <Button htmlType="submit">Login</Button>
+        </form>
     );
 };
 
