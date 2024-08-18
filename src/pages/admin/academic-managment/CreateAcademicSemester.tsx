1d3a1dd1eff1c05ef14b6d84c4d9ce008dbe25fd
raihanuldev <rihanthedev@gmail.com>
2024-08-18 09:56:15 +0600

    createAcademic

--- src/pages/admin/academic-managment/CreateAcademicSemester.tsx
+++ src/pages/admin/academic-managment/CreateAcademicSemester.tsx
@@ -1,17 +1,22 @@
-import { Button } from "antd";
+import { Button, Col, Flex } from "antd";
 import PHForm from "../../../componets/form/PHForm";
 import PHInput from "../../../componets/form/PHInput";
 
 const CreateAcademicSemester = () => {
-    const onSubmit = (data)=>{
-        console.log(data);
-    }
-    return (
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <Flex justify="center" align="center">
+      <Col span={6}>
         <PHForm onSubmit={onSubmit}>
-           <PHInput type="text" name="name"/>
-           <Button htmlType="submit">Submit</Button>
+          <PHInput type="text" name="name" label="name" />
+          <PHInput type="text" name="name" label="year" />
+          <Button htmlType="submit">Submit</Button>
         </PHForm>
-    );
+      </Col>
+    </Flex>
+  );
 };
 
 export default CreateAcademicSemester;
