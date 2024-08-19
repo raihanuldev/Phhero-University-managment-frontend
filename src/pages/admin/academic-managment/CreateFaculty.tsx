a340058294b70db48120733cc1e6bc181113f918
raihanuldev <rihanthedev@gmail.com>
2024-08-19 10:02:26 +0600

    FacultyData Vaildation

--- src/pages/admin/academic-managment/CreateFaculty.tsx
+++ src/pages/admin/academic-managment/CreateFaculty.tsx
@@ -1,16 +1,20 @@
-import React from "react";
+import { z } from "zod";
 import PHForm from "../../../componets/form/PHForm";
 import PHInput from "../../../componets/form/PHInput";
 import { Button, Col, Flex } from "antd";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const CreateFaculty = () => {
   const handleSubmit = (data) => {
     console.log(data);
   };
+  const FacultySchema = z.object({
+    name: z.string({ required_error: "Please give Faculty Name" }),
+  });
   return (
     <Flex justify="center" align="center">
       <Col span={6}>
-        <PHForm onSubmit={handleSubmit}>
+        <PHForm onSubmit={handleSubmit} resolver={zodResolver(FacultySchema)}>
           <PHInput name="name" type="text" label="Faculty Name" />
           <Button htmlType="submit">Submit</Button>
         </PHForm>
