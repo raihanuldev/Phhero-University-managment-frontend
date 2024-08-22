0abaf3f30c457610e31c2ffcbe57643af86b3de6
raihanuldev <rihanthedev@gmail.com>
2024-08-22 08:47:12 +0600

    added department

--- src/pages/admin/academic-managment/CreateADepartment.tsx
+++ src/pages/admin/academic-managment/CreateADepartment.tsx
@@ -2,26 +2,38 @@ import PHForm from "../../../componets/form/PHForm";
 import PHInput from "../../../componets/form/PHInput";
 import { Button, Col, Row } from "antd";
 import PHSelect from "../../../componets/form/PHSelect";
-import { useAcademicFacultysQuery } from "../../../redux/features/admin/academicMangement.api";
+import {
+  useAcademicFacultysQuery,
+  useAddAcademicDepartmentMutation,
+} from "../../../redux/features/admin/academicMangement.api";
 import { FieldValues, SubmitHandler } from "react-hook-form";
+import { toast } from "sonner";
 
 const CreateADepartment = () => {
-    const {data:faculties} = useAcademicFacultysQuery(undefined)
-    const [addDepartment] = 
-    const facultiesData = faculties?.data?.map((item: { _id: string; name: string; }) => ({
-        value: item._id,
-        label: `${item.name}`,
-      }));
+  const { data: faculties } = useAcademicFacultysQuery(undefined);
+  const [AddAcademicDepartment] = useAddAcademicDepartmentMutation();
+  const facultiesData = faculties?.data?.map(
+    (item: { _id: string; name: string }) => ({
+      value: item._id,
+      label: `${item.name}`,
+    })
+  );
 
-  const handleSubmit:SubmitHandler<FieldValues> = async (data) => {
-    console.log(data);
+  const handleSubmit: SubmitHandler<FieldValues> = async (data) => {
+    const res = await AddAcademicDepartment(data)
+    console.log(res);
+    toast.success("Created Succefullyy")
   };
   return (
     <Row justify="center">
       <Col span={8}>
         <PHForm onSubmit={handleSubmit}>
           <PHInput name="name" type="text" label="Name Of Department" />
-          <PHSelect label="Select Academic Faculties" name="academicFaculty" options={facultiesData}/>
+          <PHSelect
+            label="Select Academic Faculties"
+            name="academicFaculty"
+            options={facultiesData}
+          />
           <Button htmlType="submit">Submit</Button>
         </PHForm>
       </Col>
--- src/redux/features/admin/academicMangement.api.ts
+++ src/redux/features/admin/academicMangement.api.ts
@@ -85,4 +85,6 @@ export const {
   useAddAcademicSemesterMutation,
   useAddAcademicFacultyMutation,
   useAcademicFacultysQuery,
+  useAcademicDepartmentQuery,
+  useAddAcademicDepartmentMutation
 } = academicMangmentApi;
