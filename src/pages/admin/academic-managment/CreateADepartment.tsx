12dec12101f1ccdc0705fc25539e324c3e4db6c3
raihanuldev <rihanthedev@gmail.com>
2024-08-22 08:37:30 +0600

    form data collected

--- src/pages/admin/academic-managment/CreateADepartment.tsx
+++ src/pages/admin/academic-managment/CreateADepartment.tsx
@@ -2,8 +2,16 @@ import PHForm from "../../../componets/form/PHForm";
 import PHInput from "../../../componets/form/PHInput";
 import { Button, Col, Row } from "antd";
 import PHSelect from "../../../componets/form/PHSelect";
+import { useAcademicFacultysQuery } from "../../../redux/features/admin/academicMangement.api";
 
 const CreateADepartment = () => {
+    const {data:faculties} = useAcademicFacultysQuery(undefined)
+
+    const facultiesData = faculties?.data?.map((item) => ({
+        value: item._id,
+        label: `${item.name}`,
+      }));
+
   const handleSubmit = async (data) => {
     console.log(data);
   };
@@ -12,7 +20,7 @@ const CreateADepartment = () => {
       <Col span={8}>
         <PHForm onSubmit={handleSubmit}>
           <PHInput name="name" type="text" label="Name Of Department" />
-          <PHSelect label="Select Academic Faculties" name="academicFaculty" options={[{value:"adhkadha",label:"Test"}]}/>
+          <PHSelect label="Select Academic Faculties" name="academicFaculty" options={facultiesData}/>
           <Button htmlType="submit">Submit</Button>
         </PHForm>
       </Col>
