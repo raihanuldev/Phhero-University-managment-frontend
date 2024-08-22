ae3812f0817de99aed30afc4ccbfc91ede0f29bc
raihanuldev <rihanthedev@gmail.com>
2024-08-22 08:32:33 +0600

    Academic Department

--- src/pages/admin/academic-managment/CreateADepartment.tsx
+++ src/pages/admin/academic-managment/CreateADepartment.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import PHForm from "../../../componets/form/PHForm";
+import PHInput from "../../../componets/form/PHInput";
+import { Button, Col, Row } from "antd";
+import PHSelect from "../../../componets/form/PHSelect";
 
 const CreateADepartment = () => {
-    return (
-        <div>
-            Academic Department
-        </div>
-    );
+  const handleSubmit = async (data) => {
+    console.log(data);
+  };
+  return (
+    <Row justify="center">
+      <Col span={8}>
+        <PHForm onSubmit={handleSubmit}>
+          <PHInput name="name" type="text" label="Name Of Department" />
+          <PHSelect label="Select Academic Faculties" name="academicFaculty" options={[{value:"adhkadha",label:"Test"}]}/>
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Col>
+    </Row>
+  );
 };
 
 export default CreateADepartment;
